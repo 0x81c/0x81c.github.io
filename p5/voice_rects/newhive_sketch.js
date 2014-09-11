6063ba44ee223a5ddf6169654c28b01387278f01
0x81c <mmlxxvi@gmail.com>
2014-09-11 18:52:27 -0400

    ok

--- p5/voice_rects/newhive_sketch.js
+++ p5/voice_rects/newhive_sketch.js
@@ -40,7 +40,7 @@ function draw() {
   background(255);
 
   //get audio volume and map
-  var vol = input.getLevel(.3) * 10;
+  var vol = input.getLevel(.5) * 10;
   //var vol = 1;
 
   //draw gradient
@@ -64,10 +64,12 @@ function draw() {
     }
 
     strokeWeight(Quads[quad_num].thickness);
-    var adjusted_color = Quads[quad_num].line_color * vol;
-    console.log(adjusted_color);
-
-    stroke(Quads[quad_num].line_color);
+    var adjusted_color = Quads[quad_num].line_color;
+    var r = red(adjusted_color) * vol;
+    var g = green(adjusted_color) * vol;
+    var b = blue(adjusted_color) * vol;
+    var adjusted_color = color(r, g, b);
+    stroke(adjusted_color);
     quad(p[0], p[1], p[4], p[5], p[2], p[3], p[6], p[7]);
   }
 }
