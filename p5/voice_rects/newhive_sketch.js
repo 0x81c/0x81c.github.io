5539a7d7ede445490d6adc07a5733dc5ccf30b74
0x81c <mmlxxvi@gmail.com>
2014-09-11 19:43:09 -0400

    ok

--- p5/voice_rects/newhive_sketch.js
+++ p5/voice_rects/newhive_sketch.js
@@ -40,7 +40,7 @@ function draw() {
   background(255);
 
   //get audio volume and map
-  var vol = input.getLevel(.999) * 10;
+  var vol = input.getLevel(.99) * 10;
   //var vol = 1;
 
   //draw gradient
