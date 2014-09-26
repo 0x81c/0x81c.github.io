65a8de0c5acfba0044626fd05cceb326dcd8f8bb
0x81c <mmlxxvi@gmail.com>
2014-09-25 23:07:09 -0400

    ok

---
+++ p5/art/newhive_sketch.js
@@ -0,0 +1,84 @@
+/* an art by goode bye / 0x81c
+ * i will sue u if u try to rip off my algorithms!!! hackers
+ * haha jk
+ * im too poor to sue anyone
+ * please donate to my life kickstarter
+ * thanks
+*/
+
+var screen_width = 1000;
+var screen_height = 700;
+var theta = 0;
+
+
+function preload() {
+  var image_1 = Math.floor(random(0, 12));
+  var image_2 = Math.floor(random(0, 12));
+  var image_3 = Math.floor(random(0, 12));
+  var dir = ""
+  img = loadImage(dir + image_1 + ".jpg");
+  img_clone = loadImage(dir + image_2 + ".jpg");
+  img4 = loadImage(dir + image_2 + ".jpg");
+  img5 = loadImage(dir + image_3 + ".jpg");
+
+}
+
+function setup() {
+  createCanvas(screen_width, screen_height);
+}
+
+function draw() {
+  background(10, 10, 10);
+  theta += Math.PI / 10;
+  var threshold = sin(theta);
+  var threshold = map(threshold, -1, 1, .4, .6);
+  create_collage(img5, img4, img, img_clone, threshold);
+}
+
+function create_collage(img1, img2, img3, ref_image, level) {
+  ref_image.loadPixels();
+  img1.loadPixels();
+  img2.loadPixels();
+  img3.loadPixels();
+
+  var thresh = Math.floor(level * 255);
+
+  for (var x = 0; x < ref_image.width * 4; x += 4) {
+    for (var y = 0; y < ref_image.height; y += 1) {
+      
+      var r1 = ref_image.pixels[ref_image.width * 4 * y + x];
+      var g1 = ref_image.pixels[ref_image.width * 4 * y + x + 1];
+      var b1 = ref_image.pixels[ref_image.width * 4 * y + x + 2];
+      var r2, g2, b2, r3, g3, b3;
+
+
+      var grey = (0.2126 * r1 + 0.7152 * g1 + 0.0722 * b1);
+      var val;
+      if (grey >= thresh) {
+        var r2 = img2.pixels[img2.width * 4 * y + x];
+        var g2 = img2.pixels[img2.width * 4 * y + x + 1];
+        var b2 = img2.pixels[img2.width * 4 * y + x + 2];
+        img1.pixels[img1.width * 4 * y + x] = r2;
+        img1.pixels[img1.width * 4 * y + x + 1] = g2;
+        img1.pixels[img1.width * 4 * y + x + 2] = b2;
+        //console.log("white!!!");
+      } else {
+        var r3 = img3.pixels[img3.width * 4 * y + x];
+        var g3 = img3.pixels[img3.width * 4 * y + x + 1];
+        var b3 = img3.pixels[img3.width * 4 * y + x + 2];
+        img1.pixels[img1.width * 4 * y + x] = r3;
+        img1.pixels[img1.width * 4 * y + x + 1] = g3;
+        img1.pixels[img1.width * 4 * y + x + 2] = b3;
+      }
+    }
+  }
+  img1.updatePixels();
+  image(img1, 0, 0);
+}
+
+function grab_arts(number_of_arts) {
+  var image_array = [];
+  var img = loadImage("3.jpg");
+  image_array.push(img);
+  return image_array;
+}
